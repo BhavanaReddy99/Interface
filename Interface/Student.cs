81d97670f7a3143b867fa2951c015429dac41663
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 12:11:44 +0530

    Add project files.

---
+++ Interface/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Student : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfJoining { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Student Age: {Age}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Date of Joining: {DateOfJoining.ToShortDateString()}");
+        }
+    }
+}
---
+++ Interface/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Teacher : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfJoining { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Teacher Name: {Name}");
+            Console.WriteLine($"Teacher Age: {Age}");
+            Console.WriteLine($"Date of Joining: {DateOfJoining.ToShortDateString()}");
+        }
+    }
+}
